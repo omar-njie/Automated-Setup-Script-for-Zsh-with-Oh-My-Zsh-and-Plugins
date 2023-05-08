93492ff80f1e9b6e9fcc47ff2a2a67b65f8179e4
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-05-08 03:07:34 -0700

    Update

--- install-zsh.sh
+++ install-zsh.sh
@@ -24,6 +24,24 @@ fi
 echo 'Installing oh-my-zsh...'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+##################################################
+##   	   CUSTOM PLUGINS BEGIN HERE			##
+##################################################
+
+# zsh-autosuggestions
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
+
+# zsh-syntax-highlighting
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+##################################################
+##   	   CUSTOM PLUGINS END HERE		     	##
+##################################################
+
 echo 'Adding plugins to .zshrc...'
 sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
