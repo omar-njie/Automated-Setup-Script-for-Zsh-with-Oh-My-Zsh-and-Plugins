a3c4367a22260fa216eec186f5917fcbdb519b0e
El Hadji Ndiaye <66810439+omar-njie@users.noreply.github.com>
2023-05-08 22:00:03 -0700

    Update

--- install-zsh.sh
+++ install-zsh.sh
@@ -38,21 +38,14 @@ fi
 echo 'Installing oh-my-zsh...'
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   if [ -f /etc/debian_version ]; then
-    export RUNZSH=no
-    export CHSH=no
     sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
   else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	export RUNZSH=no
-	export CHSH=no
   fi
 else
   echo 'oh-my-zsh is already installed.'
 fi
 
-while [ ! -d "$HOME/.oh-my-zsh/custom" ]; do
-  sleep 1
-done
 
 ##################################################
 ##           CUSTOM PLUGINS BEGIN HERE          ##
@@ -76,5 +69,4 @@ echo 'Adding plugins to .zshrc...'
 sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
 echo 'Restarting terminal...'
-tset
-
+tset 
